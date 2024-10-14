fd3371a299b814af341c928935732970bef5571f
JordanOC <jordan.cruz@opencircle.com.br>
2024-10-14 01:55:19 -0300

    fix(MSU-00): remove logs

--- task-user-service/src/test/java/com/task/user/repository/UserRepositoryTest.java
+++ task-user-service/src/test/java/com/task/user/repository/UserRepositoryTest.java
@@ -20,7 +20,6 @@ class UserRepositoryTest {
 
     @BeforeEach
     public void setUp() {
-        // Criação de um usuário de teste
         testUser = new User();
         testUser.setName("Jordan Cruz");
         testUser.setEmail("jordan@example.com");
@@ -29,10 +28,9 @@ class UserRepositoryTest {
 
     @Test
     void testFindByEmail() {
-        // Executa o método findByEmail
+
         User foundUser = userRepository.findByEmail("jordan@example.com");
 
-        // Verifica se o usuário retornado não é nulo e se as informações estão corretas
         assertThat(foundUser).isNotNull();
         assertThat(foundUser.getEmail()).isEqualTo(testUser.getEmail());
         assertThat(foundUser.getName()).isEqualTo(testUser.getName());
@@ -40,10 +38,8 @@ class UserRepositoryTest {
 
     @Test
     void testFindByEmailNotFound() {
-        // Busca por um email que não existe
         User foundUser = userRepository.findByEmail("nonexistent@example.com");
 
-        // Verifica se o resultado é nulo
         assertThat(foundUser).isNull();
     }
 }
